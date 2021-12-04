578effef16f926edba2c6d8eeabc1441c48b16e5
Daniel Povey <dpovey@gmail.com>
2021-12-04 21:33:07 +0800

    First version of prediction code that kind of works.

--- quantization/test_train_hdf5.py
+++ quantization/test_train_hdf5.py
@@ -5,7 +5,7 @@ import torch
 from torch import nn
 from torch import Tensor
 from quantization import read_hdf5_data, Quantizer, QuantizerTrainer
-
+from prediction import JointCodebookPredictor
 
 def _test_train_from_file():
     train, valid = read_hdf5_data('training_data.hdf5')
@@ -76,6 +76,60 @@ def _test_train_from_file():
           f"it means that the data is easier to compress than if it\n"
           f"were a Gaussian with a spherical covariance matrix.")
 
+def _test_joint_predictor():
+    train, valid = read_hdf5_data('training_data.hdf5')
+    dim = train.shape[1]
+
+    device = torch.device('cuda')
+    quantizer_fn = 'quantizer.pt'
+    quantizer = Quantizer(dim=dim, num_codebooks=4, codebook_size=256)
+    quantizer.load_state_dict(torch.load(quantizer_fn))
+    quantizer = quantizer.to(device)
+
+    # bytes_per_frame is the key thing you might want to tune: e.g. try 2 or 8
+    # or 16.
+    bytes_per_frame = 4
+
+    B = 512  # Minibatch size, this is very arbitrary, it's close to what we used
+             # when we tuned this method.
+    def minibatch_generator(data: Tensor,
+                            repeat: bool):
+        assert 3 * B < data.shape[0]
+        cur_offset = 0
+        while (True if repeat else cur_offset + B <= data.shape[0]):
+            start = cur_offset % (data.shape[0] + 1 - B)
+            end = start + B
+            cur_offset += B
+            yield data[start:end,:].to(device).to(dtype=torch.float)
+
+    predictor = JointCodebookPredictor(predictor_dim=dim,
+                                       num_codebooks=bytes_per_frame).to(device)
+
+    optim = torch.optim.Adam(
+        predictor.parameters(), lr=0.0001, betas=(0.9, 0.98), eps=1e-9, weight_decay=1.0e-06
+    )
+    scheduler = torch.optim.lr_scheduler.StepLR(optim,
+                                                step_size=2000,
+                                                gamma=0.5)
+
+    count = 0
+    for x in minibatch_generator(train, repeat=True):
+        x = x.to(device)
+        encoding = quantizer.encode(x)
+        tot_logprob, tot_count = predictor(x, encoding) # should be easy to predict encoding from x.
+
+        loss = -(tot_logprob / tot_count)
+        if count % 200 == 0:
+            logging.info(f"Iter={count}, loss = {loss.item():.3f}")
+        loss.backward()
+        optim.step()
+        scheduler.step()
+        count += 1
+        if count > 10000:
+            break
+
+
 if __name__ == "__main__":
     logging.getLogger().setLevel(logging.INFO)
-    _test_train_from_file()
+    #_test_train_from_file()
+    _test_joint_predictor()
